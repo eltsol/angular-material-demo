30973363b88d37888e8d3ef8f8b0a56125d8ff3e
tsolakou <tsolakou@ics.forth.com>
2020-11-11 13:13:18 +0200

    Create multiple angular material components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-material-demo';
+  notifications = 0;
+  loadSpinner = false;
+  opened = false;
+  selectedValue: string;
+  options: string[] = ['Angular', 'React', 'Vue'];
+  minDate = new Date();
+  maxDate = new Date(2019, 3, 10);
+
+  loadData() {
+    this.loadSpinner = true;
+  }
+
+  log(state) {
+    console.log(state);
+  }
+
+  logChange(index) {
+    console.log(index);
+  }
+
+  dateFilter(date) {
+    const day = date.getDay();
+    return day !== 0 && day !== 6;
+  }
+
+  constructor(private snackBar: MatSnackBar) { }
+
+  openSnackBar(message, action) {
+    let snackBarRef = this.snackBar.open(message, action, { duration: 2000 });
+
+    snackBarRef.afterDismissed().subscribe(() => {
+      console.log('The snackbar was dismissed');
+    });
+
+    snackBarRef.onAction().subscribe(() => {
+      console.log('The snackbar action was triggered');
+    });
+  }
 }
